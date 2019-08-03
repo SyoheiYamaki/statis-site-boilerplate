887e5b29df67030bc0b736f0ca258837c8ad71c9
山木祥平 <yamaki817122@gmail.com>
2019-08-03 13:43:04 +0900

    add dom contents

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,33 @@
-// Add your javascript implements here.
-var $ = window.$;
-var $test = $(".test");
-$test.text("test");
+// $(function() {
+
+//   // parallax
+//   let shapeTop1 =  $('.js-shape-parallax-1').offset().top;
+//   let shapeTop2 =  $('.js-shape-parallax-2').offset().top;
+//   let shapeTop3 =  $('.js-shape-parallax-3').offset().top;
+//   let shapeTop4 =  $('.js-shape-parallax-4').offset().top;
+//   let shapeTop5 =  $('.js-shape-parallax-5').offset().top;
+//   let shapeTop6 =  $('.js-shape-parallax-6').offset().top;
+//   let shapeTop7 =  $('.js-shape-parallax-7').offset().top;
+//   let shapeTop8 =  $('.js-shape-parallax-8').offset().top;
+//   let shapeTop9 =  $('.js-shape-parallax-9').offset().top;
+//   let shapeTop10 =  $('.js-shape-parallax-10').offset().top;
+//   let shapeTop11 =  $('.js-shape-parallax-11').offset().top;
+//   let shapeTop12 =  $('.js-shape-parallax-12').offset().top;
+
+//   $(window).on('scroll', function() {
+//     let scrollValue = $(this).scrollTop();
+//     // console.log(scrollValue);
+//     $('.js-shape-parallax-1').css('top', shapeTop1 + scrollValue / 2 );
+//     $('.js-shape-parallax-2').css('top', shapeTop2 + scrollValue / 2 );
+//     $('.js-shape-parallax-3').css('top', shapeTop3 + scrollValue / 2 );
+//     $('.js-shape-parallax-4').css('top', shapeTop4 + scrollValue / 2 );
+//     $('.js-shape-parallax-5').css('top', shapeTop5 + scrollValue / 2 );
+//     $('.js-shape-parallax-6').css('top', shapeTop6 + scrollValue / 2 );
+//     $('.js-shape-parallax-7').css('top', shapeTop7 + scrollValue / 2 );
+//     $('.js-shape-parallax-8').css('top', shapeTop8 + scrollValue / 2 );
+//     $('.js-shape-parallax-9').css('top', shapeTop9 + scrollValue / 2 );
+//     $('.js-shape-parallax-10').css('top', shapeTop10 + scrollValue / 2 );
+//     $('.js-shape-parallax-11').css('top', shapeTop11 + scrollValue / 2 );
+//     $('.js-shape-parallax-12').css('top', shapeTop12 + scrollValue / 2 );
+//   });
+// });
